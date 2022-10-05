779843f3c13725e736a494c246c9ce658c355d2e
HM <hieumdd@gmail.com>
2022-10-05 13:11:13 +0700

    chore: Remove python

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RouterModule } from '@nestjs/core';
-import { ScheduleModule } from '@nestjs/schedule';
 
 import { DatabaseModule } from './database/database.module';
 import { AnalyticsModule } from './analytics/analytics.module';
@@ -16,7 +15,6 @@ const routes = [
 @Module({
     imports: [
         ConfigModule.forRoot(),
-        ScheduleModule.forRoot(),
         DatabaseModule,
         ...routes.map(({ module }) => module),
         RouterModule.register(routes),
